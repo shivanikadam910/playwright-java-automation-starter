01a885c619bc1ade641ea99c0590bb5db735318f
Shivani kadam <shivanikadam910@gmail.com>
2025-07-05 17:20:39 +0530

    changing URL usage

--- playwright/src/test/java/Login/LoginConstants.java
+++ playwright/src/test/java/Login/LoginConstants.java
@@ -4,13 +4,11 @@ import playwright.LaunchBrowser;
 
 public class LoginConstants  extends LaunchBrowser {
 
-	public static final String SUCCESS_MESSAGE_SELECTOR = ".woocommerce-message";
-
 	// URLs and credentials for the login page
 
-	public static final String LOGIN_URL = "https://practicetestautomation.com/practice-test-login/";
+	public static final String LOGIN_URL = prop.getProperty("url") + "practice-test-login/";
 	public static final String USERNAME = "student";
 	public static final String PASSWORD = "Password123";
-	public static final String SUCCESS_URL = "https://practicetestautomation.com/logged-in-successfully/";
+	public static final String SUCCESS_URL = prop.getProperty("url") + "logged-in-successfully/";
 
 }
